ecac118c834b9577567fb3cbf60c0c3d5b20a32b
Diego-Catalan-cmd <dcatalan2025@alu.uct.cl>
2026-05-23 19:30:41 -0300

    Fix: corregida ruta de main.js en index

--- index.php
+++ index.php
@@ -248,6 +248,6 @@ try {
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     
-    <script src="assets/main.js"></script>
+    <script src="assets/js/main.js"></script>
 </body>
 </html>
